0602ea38747ea62436166cfd0a89b4ad604a3454
Allan Neves <allan@webfloat.com.br>
2022-05-19 21:36:47 -0300

    contato validado

---
+++ js/contato/script.js
@@ -0,0 +1,47 @@
+var app = angular.module('app', []);
+
+app.controller('ContactController', ['$scope', ($scope) => {
+    var s = $scope;
+
+    s.loading = false;
+
+    s.data = {
+        name: null,
+        email: null,
+        subject: 'Sugestões',
+        message: null
+    };
+
+    s.clean = function () {
+        s.data = {
+            name: null,
+            email: null,
+            subject: 'Sugestões',
+            message: null
+        };
+    }
+
+    s.status = {
+        error: null,
+        message: null
+    }
+
+    function validateEmail (email) {
+        if (email) {
+            if (/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)) return true;
+            return false;
+        }
+        return false;
+    }
+
+    s.contact = function () {
+        if (!s.data.name) return toastr.warning('Informe um nome para continuar.');
+        if (!s.data.email) return toastr.warning('Informe um e-mail para continuar.');
+        if (!validateEmail(s.data.email)) return toastr.warning('Informe um e-mail válido.');
+        if (!s.data.subject) return toastr.warning('Informe um assunto para continuar.');
+        if (!s.data.message) return toastr.warning('Informe uma mensagem para continuar.');
+
+        toastr.success('Mensagem enviada com sucesso.');
+        s.clean();
+    }
+}]);
